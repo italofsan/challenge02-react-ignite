157eeef6911584d825529c205ab17c14e337e8f3
Italo Santos <italofsan@outlook.com>
2021-06-23 20:17:52 -0300

    Componentização dos componentes: Sidebar e Content; Criação do arquivo types onde está centralizada a tipagem da aplicação facilitando o uso para diversos componentes caso seja necessário

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "../components/Button";
+import "../styles/sidebar.scss";
+
+import { GenreResponseProps } from "../types";
+
+interface Props {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
 }
+
+function SideBar({ genres, selectedGenreId, handleClickButton }: Props) {
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
+
+export default SideBar;
